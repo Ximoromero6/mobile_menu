09c63acd90ebdac8c92a226f2694e8b07bdcaea6
Ximoromero5 <joaquin@vivaconversion.com>
2021-08-18 13:17:37 +0200

    added styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
-let menu = document.querySelector("#menu");
+let menu = document.querySelector("#menu"),
+    overlay_menu = document.querySelector(".overlay_menu_mobile");
 
 document.querySelector("#open_menu_button").addEventListener("click", () => {
+    overlay_menu.classList.remove("hidden");
     menu.classList.add("open");
 });
 
-document.querySelector(".overlay_menu_mobile").addEventListener("click", () => {
+overlay_menu.addEventListener("click", (el) => {
+    overlay_menu.classList.add("hidden");
     menu.classList.remove("open");
 });
