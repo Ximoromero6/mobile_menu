875c6efc868e900a4e8f02d2f6d903bc94091fea
Ximo Romero <67222857+Ximoromero6@users.noreply.github.com>
2021-08-18 18:50:37 +0200

    Cambios click menú

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
+(function() {
 let menu = document.querySelector("#menu"),
     overlay_menu = document.querySelector(".overlay_menu_mobile");
 
 document.querySelector("#open_menu_button").addEventListener("click", () => {
-    overlay_menu.classList.remove("hidden");
     menu.classList.add("open");
+    overlay_menu.classList.remove("hidden");
 });
 
 overlay_menu.addEventListener("click", (el) => {
-    overlay_menu.classList.add("hidden");
+    el.classList.add("hidden");
     menu.classList.remove("open");
 });
+})();
