ae93e83184fe17f86bb9b883b07c8bb0908222c8
Ximoromero5 <joaquin@vivaconversion.com>
2021-08-20 13:20:32 +0200

    added dark mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     let menu = document.querySelector("#menu"),
         overlay_menu = document.querySelector(".overlay_menu_mobile"),
         open_menu_button = document.querySelector("#open_menu_button");
@@ -12,5 +12,29 @@
         el.target.classList.add("hidden");
         menu.classList.remove("open");
     });
+
+    //Dark mode
+    const change_theme_button = document.querySelector("#switch");
+
+    change_theme_button.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+        change_theme_button.classList.toggle("active-bg");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("dark-mode", "true");
+        } else {
+            localStorage.setItem("dark-mode", "false");
+        }
+    });
+
+    if (localStorage.getItem("dark-mode") === "true") {
+        document.body.classList.add("dark");
+        change_theme_button.classList.add("active-bg");
+        change_theme_button.checked = true;
+    } else {
+        document.body.classList.remove("dark");
+        change_theme_button.classList.remove("active-bg");
+        change_theme_button.checked = false;
+    }
 
 })();
